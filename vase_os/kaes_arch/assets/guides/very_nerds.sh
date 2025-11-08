f98892b8642705a2804529b976b1b8e4e05ec9a4
h8d13 <hadrien.loge@gmail.com>
2025-11-08 10:45:26 +0100

    fix submodule

--- vase_os/kaes_arch/assets/guides/very_nerds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## remove sddm from boot
-systemctl disable sddm
-## this will drop you to a tty on next reboot
-# login with your user (or root but thats hella not recommended)
-# startplasma-wayland or systemctl start sddm
-# you can also use chvt 1, 2, 3 etc to change tty's > or ctrl + alt + f1, f2, f3, etc 
-#whenever you need graphical session and can set an alias for this & the same for stop
